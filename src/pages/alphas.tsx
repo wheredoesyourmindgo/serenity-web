343bfbc67d0a55d54f62befbfbd946545017866b
Abe Hendricks <ahendricks@pcwa.net>
2022-02-26 08:19:14 -0800

    Added Keyboard Analysis Tool stats

--- src/pages/alphas.tsx
+++ src/pages/alphas.tsx
@@ -167,6 +167,82 @@ Row Use
               </Link>
             </RowBox>
           </Box>
+
+          <Box pt={14}>
+            <Paper sx={{padding: 4}} elevation={0}>
+              <Type
+                gutterBottom
+                component="pre"
+                sx={{color: 'solarized.base01'}}
+              >
+                <em>{`Keyboard Analysis Tool - v1.32`}</em>
+              </Type>
+              <Divider />
+              <Type
+                component="pre"
+                variant="body2"
+                sx={{color: 'solarized.base00'}}
+              >
+                {`
+Finger Frequency
+finger 0 	6.95%	   finger 9 	9.13%
+finger 1 	9.70%	   finger 8 	11.30%
+finger 2 	11.89%	 finger 7 	20.28%
+finger 3 	16.90%	 finger 6 	13.84%
+------------------------------------
+total L 	45.45%	 total R 	  54.55%
+
+Finger Bigram Frequency
+finger 0 	0.001%	 finger 9 	0.016%
+finger 1 	0.085%	 finger 8 	0.144%
+finger 2 	0.018%	 finger 7 	0.171%
+finger 3 	0.253%	 finger 6 	0.057%
+------------------------------------
+                   total	    0.745%
+
+Top Same-Finger Bigrams
+finger 3 	NY 	0.106%
+finger 8 	UI 	0.097%
+finger 1 	RL 	0.075%
+finger 7 	OE 	0.048%
+finger 3 	NF 	0.043%
+
+Top Neighbour-Finger Bigrams
+finger 7-8 	OU 	1.453%
+finger 9-8 	AI 	0.475%
+finger 7-8 	E, 	0.400%
+finger 1-0 	RS 	0.359%
+finger 8-7 	IO 	0.323%
+
+Finger Effort
+ 	        base	s-bigrams	n-bigrams total
+finger 0 	0.125	0.000	    0.004	    0.129
+finger 1 	0.175	0.003	    0.002	    0.179
+finger 2 	0.163	0.001	    -0.006    0.158
+finger 3 	0.274	0.007		            0.281
+finger 6 	0.281	0.002		            0.282
+finger 7 	0.302	0.005	    -0.010	  0.298
+finger 8 	0.203	0.005	    0.004	    0.212
+finger 9 	0.168	0.000	    0.005	    0.174
+-----------------------------------------
+total * 	1.691	0.022	    -0.001	  1.713
+`}
+              </Type>
+            </Paper>
+            <RowBox justifyContent="flex-end" mt={1}>
+              <Link
+                href="https://colemakmods.github.io/mod-dh/analyze.html"
+                rel="noopener noreferrer"
+                target="_blank"
+                variant="caption"
+                sx={{
+                  color: 'text.primary'
+                }}
+              >
+                https://colemakmods.github.io/mod-dh/analyze.html
+              </Link>
+            </RowBox>
+          </Box>
         </ColumnBox>
       </Container>
     </PageLayout>
