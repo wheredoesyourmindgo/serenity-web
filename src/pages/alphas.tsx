3dc2a553fc7a57f599031b9ec9949888fc55dd9c
Abe Hendricks <ahendricks@pcwa.net>
2022-02-26 08:27:48 -0800

    remove tabs from string

--- src/pages/alphas.tsx
+++ src/pages/alphas.tsx
@@ -185,20 +185,20 @@ Row Use
               >
                 {`
 Finger Frequency
-finger 0 	6.95%	   finger 9 	9.13%
-finger 1 	9.70%	   finger 8 	11.30%
-finger 2 	11.89%	 finger 7 	20.28%
-finger 3 	16.90%	 finger 6 	13.84%
+finger 0  6.95%    finger 9   9.13%
+finger 1  9.70%    finger 8   11.30%
+finger 2  11.89%   finger 7   20.28%
+finger 3  16.90%   finger 6   13.84%
 ------------------------------------
-total L 	45.45%	 total R 	  54.55%
+total L   45.45%   total R    54.55%
 
 Finger Bigram Frequency
-finger 0 	0.001%	 finger 9 	0.016%
-finger 1 	0.085%	 finger 8 	0.144%
-finger 2 	0.018%	 finger 7 	0.171%
-finger 3 	0.253%	 finger 6 	0.057%
+finger 0  0.001%  finger 9  0.016%
+finger 1  0.085%  finger 8  0.144%
+finger 2  0.018%  finger 7  0.171%
+finger 3  0.253%  finger 6  0.057%
 ------------------------------------
-                   total	    0.745%
+                  total     0.745%
 
 Top Same-Finger Bigrams
 finger 3 	NY 	0.106%
@@ -215,17 +215,17 @@ finger 1-0 	RS 	0.359%
 finger 8-7 	IO 	0.323%
 
 Finger Effort
- 	        base	s-bigrams	n-bigrams total
-finger 0 	0.125	0.000	    0.004	    0.129
-finger 1 	0.175	0.003	    0.002	    0.179
-finger 2 	0.163	0.001	    -0.006    0.158
-finger 3 	0.274	0.007		            0.281
-finger 6 	0.281	0.002		            0.282
-finger 7 	0.302	0.005	    -0.010	  0.298
-finger 8 	0.203	0.005	    0.004	    0.212
-finger 9 	0.168	0.000	    0.005	    0.174
------------------------------------------
-total * 	1.691	0.022	    -0.001	  1.713
+ 	        base   s-bigrams  n-bigrams  total
+finger 0  0.125  0.000      0.004      0.129
+finger 1  0.175  0.003      0.002      0.179
+finger 2  0.163  0.001      -0.006     0.158
+finger 3  0.274  0.007                 0.281
+finger 6  0.281  0.002                 0.282
+finger 7  0.302  0.005      -0.010     0.298
+finger 8  0.203  0.005      0.004      0.212
+finger 9  0.168  0.000      0.005      0.174
+--------------------------------------------
+total *   1.691  0.022      -0.001     1.713
 `}
               </Type>
             </Paper>
