c7c0e673795123a280077b1af083fa3022bc4e14
Abe Hendricks <ahendricks@pcwa.net>
2022-05-25 06:48:36 -0700

    ...

--- src/components/keyboard/Alphas.tsx
+++ src/components/keyboard/Alphas.tsx
@@ -41,7 +41,7 @@ export default function AlphaKeyboard() {
         <AlphaKeyLgnd color="solarized.base00">W</AlphaKeyLgnd>
         <AlphaKeyLgnd color="solarized.base1">Z</AlphaKeyLgnd>
         <AlphaKeyLgnd color="solarized.base0">K</AlphaKeyLgnd>
-        <AlphaKeyLgnd color="solarized.base00">P</AlphaKeyLgnd>
+        <AlphaKeyLgnd color="solarized.base00">F</AlphaKeyLgnd>
         <AlphaKeyLgnd color={base03}>O</AlphaKeyLgnd>
         <AlphaKeyLgnd color="solarized.base01">U</AlphaKeyLgnd>
         <AlphaKeyLgnd>;</AlphaKeyLgnd>
@@ -68,7 +68,7 @@ export default function AlphaKeyboard() {
         <AlphaKeyLgnd color="solarized.base01">C</AlphaKeyLgnd>
         <AlphaKeyLgnd color="solarized.base0">V</AlphaKeyLgnd>
         <AlphaKeyLgnd color="solarized.base0">B</AlphaKeyLgnd>
-        <AlphaKeyLgnd color="solarized.base00">F</AlphaKeyLgnd>
+        <AlphaKeyLgnd color="solarized.base00">P</AlphaKeyLgnd>
         <AlphaKeyLgnd>'</AlphaKeyLgnd>
         <AlphaKeyLgnd>,</AlphaKeyLgnd>
         <AlphaKeyLgnd>.</AlphaKeyLgnd>
