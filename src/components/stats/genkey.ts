2b556559051497e3347d5d681bb1e11c2a1bd56a
Abe Hendricks <ahendricks@pcwa.net>
2025-11-05 11:11:14 -0800

    Update stats

--- src/components/stats/genkey.ts
+++ src/components/stats/genkey.ts
@@ -4,22 +4,22 @@ q l d w v  j f o u -
 r n t s g  y h e a i ;
 x z m c b  k p ' , .
 Missing characters: [/]
-Rolls (l): 19.13%
+Rolls (l): 19.12%
         Inward: ~13.28%
-        Outward: ~5.85%
+        Outward: ~5.84%
 Rolls (r): 24.93%
         Inward: ~5.75%
         Outward: ~19.17%
 Alternates: ~42.25%
 Onehands: ~1.90%
 Redirects: ~2.79%
-Finger Speed (weighted): [0.18 1.05 1.52 3.15 1.08 2.02 1.30 0.82]
-Finger Speed (unweighted): [0.27 3.76 7.28 17.34 5.94 9.70 4.66 1.23]
+Finger Speed (weighted): [0.21 0.99 1.52 3.15 1.08 2.02 1.30 0.82]
+Finger Speed (unweighted): [0.31 3.57 7.28 17.34 5.94 9.70 4.66 1.23]
 Highest Speed (weighted): 3.15 (LI)
 Highest Speed (unweighted): 17.34 (LI)
 Index Usage: 15.9% 11.7%
-SFBs: 0.932%
-DSFBs: 6.771%
+SFBs: 0.934%
+DSFBs: 6.769%
 LSBs: 1.24%
 Top SFBs:
         sc 0.138%       au 0.127%       ua 0.111%       eo 0.078%
@@ -29,7 +29,7 @@ Worst Bigrams:
         oe 23.001       ln 12.829       ua 12.307       dm 10.048
         sc 9.482        i. 7.824        tm 7.609        sb 7.205
 
-Score: 35.84
+Score: 35.76
 `
 
 export default genkey
