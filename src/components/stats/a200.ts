23391e30a0eacc1e2ba2429e67e7fd28bde9242e
Abe Hendricks <ahendricks@pcwa.net>
2025-11-09 10:55:42 -0800

    Correct stats

--- src/components/stats/a200.ts
+++ src/components/stats/a200.ts
@@ -9,19 +9,19 @@ x z m c b  k p ' , .
 
 Trigrams
 ========
-Alternates - Total: 37.81%
-     Rolls - Total: 38.56%   In: 18.70%   Out: 19.86%   Ratio:   0.94
-  Onehands - Total:  2.22%   In:  0.57%   Out:  1.64%   Ratio:   0.35
- Redirects - Total:  2.59%
+Alternates - Total: 37.64%
+     Rolls - Total: 38.57%   In: 18.84%   Out: 19.72%   Ratio:   0.96
+  Onehands - Total:  2.23%   In:  0.59%   Out:  1.64%   Ratio:   0.36
+ Redirects - Total:  2.48%
 
 Same Finger
 ===========
-       SFB - 3.76%         DSFB - 5.96%
+       SFB - 3.75%         DSFB - 6.22%
        SFT - 0.13%          SFR - 8.98%
 
 Finger Use
 ==========
-      Left - Total: 46.48%   LP:  6.85%   LR: 11.58%   LM: 11.55%   LI: 16.50%
+      Left - Total: 46.48%   LP:  6.72%   LR: 11.71%   LM: 11.55%   LI: 16.50%
      Right - Total: 44.06%   RP:  8.19%   RR: 10.71%   RM: 16.33%   RI:  8.83%
      Thumb - Total:  9.46%
 
--- src/components/stats/colemakmods.ts
+++ src/components/stats/colemakmods.ts
@@ -1,42 +1,42 @@
 const colemakmods = `
 Finger Frequency
-finger 0 	6.79%	finger 9 	8.29%
-finger 1 	9.60%	finger 8 	12.34%
-finger 2 	15.35%	finger 7 	20.23%
-finger 3 	14.84%	finger 6 	12.56%
+finger 0 	5.72%	 finger 9 	8.29%
+finger 1 	10.67%	 finger 8 	12.34%
+finger 2 	15.35%	 finger 7 	20.23%
+finger 3 	14.84%	 finger 6 	12.56%
 total L 	46.58%	total R 	53.42%
 
 Finger Bigram Frequency
-finger 0 	0.018%	 finger 9 	0.029%
-finger 1 	0.087%	 finger 8 	0.221%
+finger 0 	0.003%	 finger 9 	0.029%
+finger 1 	0.095%	 finger 8 	0.221%
 finger 2 	0.034%	 finger 7 	0.171%
 finger 3 	0.290%	 finger 6 	0.142%
-total	0.992%
+total	0.985%
 
 Top Same-Finger Bigrams
 finger 8 	AU 	0.108%
 finger 3 	SC 	0.096%
-finger 1 	RL 	0.075%
+finger 1 	NL 	0.079%
 finger 8 	UA 	0.065%
 finger 3 	GS 	0.058%
 
 Top Neighbour-Finger Bigrams
+finger 1-2 	ND 	1.520%
 finger 7-8 	OU 	1.446%
 finger 7-8 	EA 	0.770%
+finger 1-2 	NT 	0.737%
 finger 8-9 	AI 	0.473%
-finger 7-8 	E, 	0.398%
-finger 1-2 	LD 	0.361%
 
 Finger Effort
 		base	s-bgrms	n-bgrms	total
-finger 0 	0.112	0.001	0.002	0.115
-finger 1 	0.172	0.003	0.003	0.178
+finger 0 	0.095	0.000	0.002	0.097
+finger 1 	0.186	0.003	0.005	0.194
 finger 2 	0.238	0.001	-0.003	0.236
 finger 3 	0.276	0.008		0.284
 finger 6 	0.217	0.004		0.221
 finger 7 	0.301	0.005	-0.013	0.294
 finger 8 	0.216	0.007	0.004	0.227
 finger 9 	0.160	0.001	0.005	0.166
-total * 	1.693	0.030	-0.003	1.720
+total * 	1.690	0.030	-0.001	 1.719
 `
 export default colemakmods
